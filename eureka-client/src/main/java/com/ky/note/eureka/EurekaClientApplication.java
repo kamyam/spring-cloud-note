cc5bb600545378debb8101d35a9903300e214c91
wuganqin <wuganqin@tuandai.com>
2019-05-29 09:49:33 +0800

    +注释

--- eureka-client/src/main/java/com/ky/note/eureka/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/ky/note/eureka/EurekaClientApplication.java
@@ -7,12 +7,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 /**
- * 使用@EnableEurekaClient或@EnableDiscoveryClient都可以，非Eureka建议是使用@EnableDiscoveryClient
+ * 1.
+ * 2. @EnableDiscoveryClient：使用@EnableEurekaClient或@EnableDiscoveryClient都可以;当注册中心非Eureka时必需使用@EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaClientApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
     }
