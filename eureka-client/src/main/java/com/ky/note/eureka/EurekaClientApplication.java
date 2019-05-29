6a7925ac11d50567e5da3c0cc38bba2158d1918e
wuganqin <wuganqin@tuandai.com>
2019-05-29 17:18:28 +0800

    Hystrix测试

--- eureka-client/src/main/java/com/ky/note/eureka/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/ky/note/eureka/EurekaClientApplication.java
@@ -2,11 +2,8 @@ package com.ky.note.eureka;
 
 
 import feign.Logger;
-import feign.Request;
-import feign.Retryer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
--- eureka-client/src/main/java/com/ky/note/eureka/controller/EurekaController.java
+++ eureka-client/src/main/java/com/ky/note/eureka/controller/EurekaController.java
@@ -2,15 +2,19 @@ package com.ky.note.eureka.controller;
 
 import com.ky.note.eureka.client.UserClient;
 import com.ky.user.api.dto.UserInfoDTO;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -19,20 +23,36 @@ public class EurekaController {
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private DiscoveryClient client;
+    private DiscoveryClient discoveryClient;
     @Autowired
     private Environment environment;
     @Autowired
     private UserClient userClient;
 
+    @GetMapping("/info")
+    public List<ServiceInstance> getEurekaInfo() {
+        String name = environment.getProperty("spring.application.name");
+        List<ServiceInstance> instances = discoveryClient.getInstances(name);
+        log.info("{}:当前的执行的线程", Thread.currentThread().getName());
+        return instances;
+    }
 
 
+    @HystrixCommand(fallbackMethod = "getUserInfoDefault")
     @GetMapping("/user/{userId}")
-    public UserInfoDTO info(@PathVariable String userId) {
+    public UserInfoDTO getUserInfo(@PathVariable String userId) {
         String port = environment.getProperty("server.port");
-        log.info("当前的服务端口:{}", port);
+        log.info("{}:当前的服务端口:{}", Thread.currentThread().getName(), port);
         return userClient.user(userId);
     }
 
+
+    public UserInfoDTO getUserInfoDefault(String userId) {
+        log.info("{}:进入服务降级方法", Thread.currentThread().getName());
+        UserInfoDTO userInfoDTO = new UserInfoDTO();
+        userInfoDTO.setUserId("0");
+        userInfoDTO.setUsername("系统异常");
+        return userInfoDTO;
+    }
 
 }
