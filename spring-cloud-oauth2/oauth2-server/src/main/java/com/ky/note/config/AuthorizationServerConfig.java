5fb8e1eac00205ea0be8c2c8f70597fd6d3fd345
kamyam <kamyam@163.com>
2019-06-18 17:56:53 +0800

    admin

--- spring-cloud-oauth2/oauth2-server/src/main/java/com/ky/note/config/AuthorizationServerConfig.java
+++ spring-cloud-oauth2/oauth2-server/src/main/java/com/ky/note/config/AuthorizationServerConfig.java
@@ -3,7 +3,6 @@ package com.ky.note.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -11,10 +10,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
+
 import javax.sql.DataSource;
-import java.util.Base64;
 
 /**
  * @author kamyam
@@ -40,20 +41,40 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         this.userDetailsService = userDetailsService;
     }
 
+    /**
+     * 将token存储在oauth_access_token
+     */
     @Bean
     public JdbcTokenStore jdbcTokenStore() {
         return new JdbcTokenStore(dataSource);
     }
 
 
+    @Bean
+    public JdbcAuthorizationCodeServices jdbcAuthorizationCodeServices() {
+        return new JdbcAuthorizationCodeServices(dataSource);
+    }
+
+
+    @Bean
+    public JdbcApprovalStore jdbcApprovalStore() {
+        return new JdbcApprovalStore(dataSource);
+    }
+
     /**
      * 1.当grant_types = password，必须注入AuthenticationManager
-     * 2.
+     * 2.TokenStore: 默认InMemoryTokenStore，支持JdbcTokenStore，RedisTokenStore，JwkTokenStore，JwtTokenStore
+     * 3.UserDetailsService 刷新token必需
      */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService).tokenStore(jdbcTokenStore());
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
+                .tokenStore(jdbcTokenStore())
+                .authorizationCodeServices(jdbcAuthorizationCodeServices())
+                .approvalStore(jdbcApprovalStore());
     }
+
 
     /**
      * 配置获取客户端的方式，提供了inMemory（从内存获取）和jdbc（从数据库获取）两种方式
@@ -64,11 +85,24 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
     }
 
-  /*  @Override
+
+    /**
+     * 1.allowFormAuthenticationForClients：
+     * 调用/oauth/token获取access_token的时候需要传参数client_id和client_secret
+     * 默认在请求头中获取：Authorization Basic Zmlyc3QtY2xpZW50Om5vb25ld2lsbGV2ZXJndWVzcw==
+     * 如果想从请求参数中获取需要配置allowFormAuthenticationForClients = true;开启clientCredentialsTokenEndpointFilter
+     * 2.tokenKeyAccess:
+     * 开放/oauth/token_key使用JWT的时候用来回去公钥
+     * 3.checkTokenAccess：
+     * 开放/oauth/check_token验证token是否有效
+     */
+    @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()")
-                .allowFormAuthenticationForClients();
-    }*/
+        security.allowFormAuthenticationForClients()
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("permitAll()");
+
+    }
 
 
 }
--- spring-cloud-oauth2/oauth2-server/src/main/java/com/ky/note/config/WebSecurityConfig.java
+++ spring-cloud-oauth2/oauth2-server/src/main/java/com/ky/note/config/WebSecurityConfig.java
@@ -3,14 +3,12 @@ package com.ky.note.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
@@ -35,7 +33,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-
     @Bean
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
@@ -56,11 +53,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-    // 拦截所有请求,使用httpBasic方式登陆
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").fullyAuthenticated().and().httpBasic();
-    }
 
 }
 
